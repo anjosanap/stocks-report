ab59b559223e52306a4b7bddab71d9cd320f14ef
Ana <anjosanapaula@outlook.com>
2022-09-26 18:04:59 -0300

    (In Test) Lambda to integrate with Twilio.

---
+++ functions/twilio/src/app.py
@@ -0,0 +1,30 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+
+account_sid = os.getenv('ACCOUNT_SID')
+auth_token = os.getenv('AUTH_TOKEN')
+flow_token = os.getenv('FLOW_TOKEN')
+
+def lambda_handler(event, context):
+    phone_number = event['request']['userAttributes']['phone_number']
+    name = event['request']['userAttributes']['name']
+    
+    url = f'https://studio.twilio.com/v2/Flows/{flow_token}/Executions'
+    
+    payload = {
+      "From": "+12487280914",
+      "To": phone_number,
+      "Parameters": {
+          "name": name,
+          "phone_number": phone_number
+      }
+    }
+    headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    
+    response = requests.request('POST', url, auth=HTTPBasicAuth(account_sid, auth_token), headers=headers, data=payload)
+    print(response)
+    
+    return event
